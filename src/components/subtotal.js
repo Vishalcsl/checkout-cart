3748e2fbd78361de17a73ef86a6a1dbd205cd0fc
Vishalcsl <vishalgautamcsl@gmail.com>
2019-01-27 14:55:40 +0530

    subtotal component completed

--- src/components/subtotal.js
+++ src/components/subtotal.js
@@ -2,17 +2,11 @@ import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 
 class SubTotal extends Component {
-  constructor() {
-    super();
-    this.state = {
-      subtotal: 90
-    };
-  }
   render() {
     return (
       <Row className="show-grid">
         <Col md={6}>Subtotal:-</Col>
-        <Col md={6}>{`$${this.props.subtotal}`}</Col>
+        <Col md={6}>{`Rs${this.props.subtotal}`}</Col>
       </Row>
     );
   }
